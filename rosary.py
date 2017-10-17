ce39094aaa16f8608b6f2d8255e7895e98771505
Luke Caputo <mail@lukecaputo.com>
2017-10-16 21:07:54 -0400

    Initial commit.

---
+++ rosary.py
@@ -0,0 +1,447 @@
+import datetime
+
+#
+# WIP:
+#
+# Add the decades for each mystery
+#
+# Add the closing prayers
+#
+
+weekday = datetime.datetime.today().weekday()
+
+if weekday == 0 or weekday == 5:
+    # Joyful Mysteries
+    print("\n" * 100)
+    print("""
+
+        The Joyful Mysteries
+
+
+        """)
+    input("Press Enter to Continue")
+    print("\n" * 100)
+
+    print("""
+    
+        I believe in God, the Father Almighty, Creator of heaven and 
+        earth; and in Jesus Christ, His only Son, our Lord; Who was 
+        conceived by the Holy Spirit, born of the Virgin Mary, 
+        suffered under Pontius Pilate, was crucified, died, and was 
+        buried. He descended into hell; the third day He arose again 
+        from the dead. He ascended into heaven, and sits at the right 
+        hand of God, the Father Almighty; from thence He shall come 
+        to judge the living and the dead. I believe in the Holy 
+        Spirit, the Holy Catholic Church, the communion of Saints, 
+        the forgiveness of sins, the resurrection of the body and 
+        life everlasting. Amen. 
+
+
+        """)
+    input("Press Enter to Continue")
+    print("\n" * 100)
+
+    print("""
+    
+        Our Father who art in Heaven, 
+        Hallowed be thy name; 
+        Thy kingdom come 
+        Thy will be done 
+        On earth as it is in heaven. 
+        Give us this day our daily bread; 
+        And forgive us our trespasses 
+        As we forgive those who trespass against us; 
+        And lead us not into temptation, 
+        But deliver us from evil.
+    
+
+        """)
+    input("Press Enter to Continue")
+    print("\n" * 100)
+
+    print("""
+
+        Hail Mary, full of grace.
+        Our Lord is with thee.
+        Blessed art thou among women,
+        and blessed is the fruit of thy womb, Jesus.
+        Holy Mary, Mother of God,
+        pray for us sinners,
+        now and at the hour of our death.
+        Amen. 
+
+
+    """)
+    input("Press Enter to Continue")
+    print("\n" * 100)
+
+    print("""
+
+        Hail Mary, full of grace.
+        Our Lord is with thee.
+        Blessed art thou among women,
+        and blessed is the fruit of thy womb, Jesus.
+        Holy Mary, Mother of God,
+        pray for us sinners,
+        now and at the hour of our death.
+        Amen. 
+
+
+    """)
+    input("Press Enter to Continue")
+    print("\n" * 100)
+
+    print("""
+
+        Hail Mary, full of grace.
+        Our Lord is with thee.
+        Blessed art thou among women,
+        and blessed is the fruit of thy womb, Jesus.
+        Holy Mary, Mother of God,
+        pray for us sinners,
+        now and at the hour of our death.
+        Amen. 
+
+
+    """)
+    input("Press Enter to Continue")
+    print("\n" * 100)
+
+    print("""
+    
+        Glory be to the Father,
+        And to the Son,
+        And to the Holy Spirit.
+        As it was in the beginning, is now,
+        And ever shall be,
+        World without end. Amen.
+    
+
+    """)
+
+elif weekday == 1 or weekday == 4:
+    # Sorrowful Mysteries
+    print("\n" * 100)
+    print("""
+        
+        The Sorrowful Mysteries
+
+
+        """)
+    input("Press Enter to Continue")
+    print("\n" * 100)
+
+    print("""
+    
+        I believe in God, the Father Almighty, Creator of heaven and 
+        earth; and in Jesus Christ, His only Son, our Lord; Who was 
+        conceived by the Holy Spirit, born of the Virgin Mary, 
+        suffered under Pontius Pilate, was crucified, died, and was 
+        buried. He descended into hell; the third day He arose again 
+        from the dead. He ascended into heaven, and sits at the right 
+        hand of God, the Father Almighty; from thence He shall come 
+        to judge the living and the dead. I believe in the Holy 
+        Spirit, the Holy Catholic Church, the communion of Saints, 
+        the forgiveness of sins, the resurrection of the body and 
+        life everlasting. Amen. 
+
+
+        """)
+    input("Press Enter to Continue")
+    print("\n" * 100)
+
+    print("""
+    
+        Our Father who art in Heaven, 
+        Hallowed be thy name; 
+        Thy kingdom come 
+        Thy will be done 
+        On earth as it is in heaven. 
+        Give us this day our daily bread; 
+        And forgive us our trespasses 
+        As we forgive those who trespass against us; 
+        And lead us not into temptation, 
+        But deliver us from evil.
+    
+
+        """)
+    input("Press Enter to Continue")
+    print("\n" * 100)
+
+    print("""
+
+        Hail Mary, full of grace.
+        Our Lord is with thee.
+        Blessed art thou among women,
+        and blessed is the fruit of thy womb, Jesus.
+        Holy Mary, Mother of God,
+        pray for us sinners,
+        now and at the hour of our death.
+        Amen. 
+
+
+    """)
+    input("Press Enter to Continue")
+    print("\n" * 100)
+
+    print("""
+
+        Hail Mary, full of grace.
+        Our Lord is with thee.
+        Blessed art thou among women,
+        and blessed is the fruit of thy womb, Jesus.
+        Holy Mary, Mother of God,
+        pray for us sinners,
+        now and at the hour of our death.
+        Amen. 
+
+
+    """)
+    input("Press Enter to Continue")
+    print("\n" * 100)
+
+    print("""
+
+        Hail Mary, full of grace.
+        Our Lord is with thee.
+        Blessed art thou among women,
+        and blessed is the fruit of thy womb, Jesus.
+        Holy Mary, Mother of God,
+        pray for us sinners,
+        now and at the hour of our death.
+        Amen. 
+
+
+    """)
+    input("Press Enter to Continue")
+    print("\n" * 100)
+
+    print("""
+    
+        Glory be to the Father,
+        And to the Son,
+        And to the Holy Spirit.
+        As it was in the beginning, is now,
+        And ever shall be,
+        World without end. Amen.
+    
+
+    """)
+
+elif weekday == 2 or weekday == 6:
+    # Glorious Mysteries
+    print("\n" * 100)
+    print("""
+
+        The Glorious Mysteries
+
+
+        """)
+    input("Press Enter to Continue")
+    print("\n" * 100)
+
+    print("""
+    
+        I believe in God, the Father Almighty, Creator of heaven and 
+        earth; and in Jesus Christ, His only Son, our Lord; Who was 
+        conceived by the Holy Spirit, born of the Virgin Mary, 
+        suffered under Pontius Pilate, was crucified, died, and was 
+        buried. He descended into hell; the third day He arose again 
+        from the dead. He ascended into heaven, and sits at the right 
+        hand of God, the Father Almighty; from thence He shall come 
+        to judge the living and the dead. I believe in the Holy 
+        Spirit, the Holy Catholic Church, the communion of Saints, 
+        the forgiveness of sins, the resurrection of the body and 
+        life everlasting. Amen. 
+
+
+        """)
+    input("Press Enter to Continue")
+    print("\n" * 100)
+
+    print("""
+    
+        Our Father who art in Heaven, 
+        Hallowed be thy name; 
+        Thy kingdom come 
+        Thy will be done 
+        On earth as it is in heaven. 
+        Give us this day our daily bread; 
+        And forgive us our trespasses 
+        As we forgive those who trespass against us; 
+        And lead us not into temptation, 
+        But deliver us from evil.
+    
+
+        """)
+    input("Press Enter to Continue")
+    print("\n" * 100)
+
+    print("""
+
+        Hail Mary, full of grace.
+        Our Lord is with thee.
+        Blessed art thou among women,
+        and blessed is the fruit of thy womb, Jesus.
+        Holy Mary, Mother of God,
+        pray for us sinners,
+        now and at the hour of our death.
+        Amen. 
+
+
+    """)
+    input("Press Enter to Continue")
+    print("\n" * 100)
+
+    print("""
+
+        Hail Mary, full of grace.
+        Our Lord is with thee.
+        Blessed art thou among women,
+        and blessed is the fruit of thy womb, Jesus.
+        Holy Mary, Mother of God,
+        pray for us sinners,
+        now and at the hour of our death.
+        Amen. 
+
+
+    """)
+    input("Press Enter to Continue")
+    print("\n" * 100)
+
+    print("""
+
+        Hail Mary, full of grace.
+        Our Lord is with thee.
+        Blessed art thou among women,
+        and blessed is the fruit of thy womb, Jesus.
+        Holy Mary, Mother of God,
+        pray for us sinners,
+        now and at the hour of our death.
+        Amen. 
+
+
+    """)
+    input("Press Enter to Continue")
+    print("\n" * 100)
+
+    print("""
+    
+        Glory be to the Father,
+        And to the Son,
+        And to the Holy Spirit.
+        As it was in the beginning, is now,
+        And ever shall be,
+        World without end. Amen.
+    
+
+    """)
+
+else:
+    # Luminous Mysteries
+    print("\n" * 100)
+    print("""
+
+        The Luminous Mysteries
+
+        """)
+    input("Press Enter to Continue")
+    print("\n" * 100)
+
+    print("""
+    
+        I believe in God, the Father Almighty, Creator of heaven and 
+        earth; and in Jesus Christ, His only Son, our Lord; Who was 
+        conceived by the Holy Spirit, born of the Virgin Mary, 
+        suffered under Pontius Pilate, was crucified, died, and was 
+        buried. He descended into hell; the third day He arose again 
+        from the dead. He ascended into heaven, and sits at the right 
+        hand of God, the Father Almighty; from thence He shall come 
+        to judge the living and the dead. I believe in the Holy 
+        Spirit, the Holy Catholic Church, the communion of Saints, 
+        the forgiveness of sins, the resurrection of the body and 
+        life everlasting. Amen. 
+
+
+        """)
+    input("Press Enter to Continue")
+    print("\n" * 100)
+
+    print("""
+    
+        Our Father who art in Heaven, 
+        Hallowed be thy name; 
+        Thy kingdom come 
+        Thy will be done 
+        On earth as it is in heaven. 
+        Give us this day our daily bread; 
+        And forgive us our trespasses 
+        As we forgive those who trespass against us; 
+        And lead us not into temptation, 
+        But deliver us from evil.
+    
+
+        """)
+    input("Press Enter to Continue")
+    print("\n" * 100)
+
+    print("""
+
+        Hail Mary, full of grace.
+        Our Lord is with thee.
+        Blessed art thou among women,
+        and blessed is the fruit of thy womb, Jesus.
+        Holy Mary, Mother of God,
+        pray for us sinners,
+        now and at the hour of our death.
+        Amen. 
+
+
+    """)
+    input("Press Enter to Continue")
+    print("\n" * 100)
+
+    print("""
+
+        Hail Mary, full of grace.
+        Our Lord is with thee.
+        Blessed art thou among women,
+        and blessed is the fruit of thy womb, Jesus.
+        Holy Mary, Mother of God,
+        pray for us sinners,
+        now and at the hour of our death.
+        Amen. 
+
+
+    """)
+    input("Press Enter to Continue")
+    print("\n" * 100)
+
+    print("""
+
+        Hail Mary, full of grace.
+        Our Lord is with thee.
+        Blessed art thou among women,
+        and blessed is the fruit of thy womb, Jesus.
+        Holy Mary, Mother of God,
+        pray for us sinners,
+        now and at the hour of our death.
+        Amen. 
+
+
+    """)
+    input("Press Enter to Continue")
+    print("\n" * 100)
+
+    print("""
+    
+        Glory be to the Father,
+        And to the Son,
+        And to the Holy Spirit.
+        As it was in the beginning, is now,
+        And ever shall be,
+        World without end. Amen.
+    
+
+    """)
+
