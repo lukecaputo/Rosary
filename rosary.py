e548375123fc33eb958421d2c75f2d7bb4ee3401
Luke Caputo <mail@lukecaputo.com>
2017-10-19 21:29:25 -0400

    Make all repeatables into variables. Fix some formating.

--- rosary.py
+++ rosary.py
@@ -4,10 +4,87 @@ import datetime
 # WIP:
 #
 # Add the decades for each mystery
-# Clean up formatting throughout
 #
 
 weekday = datetime.datetime.today().weekday()
+
+creed = """
+    
+    I believe in God, the Father Almighty, Creator of heaven and 
+    earth; and in Jesus Christ, His only Son, our Lord; Who was 
+    conceived by the Holy Spirit, born of the Virgin Mary, 
+    suffered under Pontius Pilate, was crucified, died, and was 
+    buried. He descended into hell; the third day He arose again 
+    from the dead. He ascended into heaven, and sits at the right 
+    hand of God, the Father Almighty; from thence He shall come 
+    to judge the living and the dead. I believe in the Holy 
+    Spirit, the Holy Catholic Church, the communion of Saints, 
+    the forgiveness of sins, the resurrection of the body and 
+    life everlasting. Amen. 
+
+
+    """
+
+ourFather = """
+    
+    Our Father who art in Heaven, Hallowed be thy name; 
+    Thy kingdom come, Thy will be done on earth as it is in heaven. 
+    Give us this day our daily bread, qnd forgive us our trespasses 
+    As we forgive those who trespass against us; 
+    And lead us not into temptation, but deliver us from evil.
+    Amen.
+    
+
+    """
+
+hailMary = """
+
+    Hail Mary, full of grace. The Lord is with thee.
+    Blessed art thou among women, and blessed is the fruit of thy womb, Jesus.
+    Holy Mary, Mother of God, pray for us sinners,
+    now and at the hour of our death. Amen. 
+
+
+    """
+
+gloryBe = """
+    
+    Glory be to the Father, and to the Son, and to the Holy Spirit.
+    As it was in the beginning, is now, and ever shall be, qorld without end.
+    Amen.
+    
+
+    """
+
+salveRegina = """
+
+    Hail, holy Queen, Mother of Mercy!
+    Our life, our sweetness, and our hope!
+    To thee do we cry, poor banished children of Eve, 
+       to thee do we send up our sighs, mourning and weeping 
+       in this valley, of tears.
+    Turn, then, most gracious advocate, thine eyes of 
+       mercy toward us; and after this our exile show unto us 
+       the blessed fruit of thy womb Jesus;
+    O clement, O loving, O sweet virgin Mary.
+            
+    Pray for us, O holy Mother of God
+    That we may be made worthy of the promises of Christ.
+
+
+    """
+
+closingPrayer = """
+
+    O God whose only begotten Son by His Life has purchased 
+       for us the rewards of eternal life, Grant that we beseech 
+       Thee while meditating upon these mysteries of the Most Holy 
+       Rosary of the Blessed Virgin Mary, we may both imitate what 
+       they contain and obtain what they promise, through the same 
+       Christ our Lord Amen.
+
+
+    """
 
 if weekday == 0 or weekday == 5:
     # Joyful Mysteries
@@ -21,102 +98,29 @@ if weekday == 0 or weekday == 5:
     input("Press Enter to Continue")
     print("\n" * 100)
 
-    print("""
-    
-    I believe in God, the Father Almighty, Creator of heaven and 
-    earth; and in Jesus Christ, His only Son, our Lord; Who was 
-    conceived by the Holy Spirit, born of the Virgin Mary, 
-    suffered under Pontius Pilate, was crucified, died, and was 
-    buried. He descended into hell; the third day He arose again 
-    from the dead. He ascended into heaven, and sits at the right 
-    hand of God, the Father Almighty; from thence He shall come 
-    to judge the living and the dead. I believe in the Holy 
-    Spirit, the Holy Catholic Church, the communion of Saints, 
-    the forgiveness of sins, the resurrection of the body and 
-    life everlasting. Amen. 
-
-
-        """)
+    print(creed)
     input("Press Enter to Continue")
     print("\n" * 100)
 
-    print("""
-    
-    Our Father who art in Heaven, 
-    Hallowed be thy name; 
-    Thy kingdom come 
-    Thy will be done 
-    On earth as it is in heaven. 
-    Give us this day our daily bread; 
-    And forgive us our trespasses 
-    As we forgive those who trespass against us; 
-    And lead us not into temptation, 
-    But deliver us from evil.
-    
-
-        """)
+    print(ourFather)
     input("Press Enter to Continue")
     print("\n" * 100)
 
-    print("""
-
-    Hail Mary, full of grace.
-    Our Lord is with thee.
-    Blessed art thou among women,
-    and blessed is the fruit of thy womb, Jesus.
-    Holy Mary, Mother of God,
-    pray for us sinners,
-    now and at the hour of our death.
-    Amen. 
-
-
-    """)
+    print(hailMary)
     input("Press Enter to Continue")
     print("\n" * 100)
 
-    print("""
-
-    Hail Mary, full of grace.
-    Our Lord is with thee.
-    Blessed art thou among women,
-    and blessed is the fruit of thy womb, Jesus.
-    Holy Mary, Mother of God,
-    pray for us sinners,
-    now and at the hour of our death.
-    Amen. 
-
-
-    """)
+    print(hailMary)
     input("Press Enter to Continue")
     print("\n" * 100)
 
-    print("""
-
-    Hail Mary, full of grace.
-    Our Lord is with thee.
-    Blessed art thou among women,
-    and blessed is the fruit of thy womb, Jesus.
-    Holy Mary, Mother of God,
-    pray for us sinners,
-    now and at the hour of our death.
-    Amen. 
-
-
-    """)
+    print(hailMary)
     input("Press Enter to Continue")
     print("\n" * 100)
 
-    print("""
-    
-    Glory be to the Father,
-    And to the Son,
-    And to the Holy Spirit.
-    As it was in the beginning, is now,
-    And ever shall be,
-    World without end. Amen.
-    
-
-    """)
+    print(gloryBe)
+    input("Press Enter to Continue")
+    print("\n" * 100)
 
     #
     # ADD THE FIVE DECADES
@@ -125,39 +129,11 @@ if weekday == 0 or weekday == 5:
     input("Press Enter to Continue")
     print("\n" * 100)
 
-    print("""
-
-    Hail, Holy Queen
-    Hail, holy Queen, Mother of Mercy!
-    Our life, our sweetness, and our hope!
-    To thee do we cry, poor banished children of Eve, 
-       to thee do we send up our sighs, mourning and weeping 
-       in this valley, of tears.
-    Turn, then, most gracious advocate, thine eyes of 
-       mercy toward us; and after this our exile show unto us 
-       the blessed fruit of thy womb Jesus;
-    O clement, O loving, O sweet virgin Mary.
-            
-    Pray for us, O holy Mother of God
-    That we may be made worthy of the promises of Christ.
-
-
-    """)
-
+    print(salveRegina)
     input("Press Enter to Continue")
     print("\n" * 100)
 
-    print("""
-
-    O God whose only begotten Son by His Life has purchased 
-       for us the rewards of eternal life, Grant that we beseech 
-       Thee while meditating upon these mysteries of the Most Holy 
-       Rosary of the Blessed Virgin Mary, we may both imitate what 
-       they contain and obtain what they promise, through the same 
-       Christ our Lord Amen.
-
-
-       """)
+    print(closingPrayer)
 
 elif weekday == 1 or weekday == 4:
     # Sorrowful Mysteries
@@ -171,144 +147,42 @@ elif weekday == 1 or weekday == 4:
     input("Press Enter to Continue")
     print("\n" * 100)
 
-    print("""
-    
-    I believe in God, the Father Almighty, Creator of heaven and 
-    earth; and in Jesus Christ, His only Son, our Lord; Who was 
-    conceived by the Holy Spirit, born of the Virgin Mary, 
-    suffered under Pontius Pilate, was crucified, died, and was 
-    buried. He descended into hell; the third day He arose again 
-    from the dead. He ascended into heaven, and sits at the right 
-    hand of God, the Father Almighty; from thence He shall come 
-    to judge the living and the dead. I believe in the Holy 
-    Spirit, the Holy Catholic Church, the communion of Saints, 
-    the forgiveness of sins, the resurrection of the body and 
-    life everlasting. Amen. 
-
-
-        """)
+    print(creed)
     input("Press Enter to Continue")
     print("\n" * 100)
 
-    print("""
-    
-    Our Father who art in Heaven, 
-    Hallowed be thy name; 
-    Thy kingdom come 
-    Thy will be done 
-    On earth as it is in heaven. 
-    Give us this day our daily bread; 
-    And forgive us our trespasses 
-    As we forgive those who trespass against us; 
-    And lead us not into temptation, 
-    But deliver us from evil.
-    
-
-        """)
+    print(ourFather)
     input("Press Enter to Continue")
     print("\n" * 100)
 
-    print("""
-
-    Hail Mary, full of grace.
-    Our Lord is with thee.
-    Blessed art thou among women,
-    and blessed is the fruit of thy womb, Jesus.
-    Holy Mary, Mother of God,
-    pray for us sinners,
-    now and at the hour of our death.
-    Amen. 
-
-
-    """)
+    print(hailMary)
     input("Press Enter to Continue")
     print("\n" * 100)
 
-    print("""
-
-    Hail Mary, full of grace.
-    Our Lord is with thee.
-    Blessed art thou among women,
-    and blessed is the fruit of thy womb, Jesus.
-    Holy Mary, Mother of God,
-    pray for us sinners,
-    now and at the hour of our death.
-    Amen. 
-
-
-    """)
+    print(hailMary)
     input("Press Enter to Continue")
     print("\n" * 100)
 
-    print("""
-
-    Hail Mary, full of grace.
-    Our Lord is with thee.
-    Blessed art thou among women,
-    and blessed is the fruit of thy womb, Jesus.
-    Holy Mary, Mother of God,
-    pray for us sinners,
-    now and at the hour of our death.
-    Amen. 
-
-
-    """)
+    print(hailMary)
     input("Press Enter to Continue")
     print("\n" * 100)
 
-    print("""
-    
-    Glory be to the Father,
-    And to the Son,
-    And to the Holy Spirit.
-    As it was in the beginning, is now,
-    And ever shall be,
-    World without end. Amen.
-    
-
-    """)
+    print(gloryBe)
+    input("Press Enter to Continue")
+    print("\n" * 100)
 
     #
     # ADD THE FIVE DECADES
     #
-
     
     input("Press Enter to Continue")
     print("\n" * 100)
 
-    print("""
-
-    Hail, Holy Queen
-    Hail, holy Queen, Mother of Mercy!
-    Our life, our sweetness, and our hope!
-    To thee do we cry, poor banished children of Eve, 
-       to thee do we send up our sighs, mourning and weeping 
-       in this valley, of tears.
-    Turn, then, most gracious advocate, thine eyes of 
-       mercy toward us; and after this our exile show unto us 
-       the blessed fruit of thy womb Jesus;
-    O clement, O loving, O sweet virgin Mary.
-            
-    Pray for us, O holy Mother of God
-    That we may be made worthy of the promises of Christ.
-
-
-    """)
-
+    print(salveRegina)
     input("Press Enter to Continue")
     print("\n" * 100)
 
-    print("""
-
-    O God whose only begotten Son by His Life has purchased 
-       for us the rewards of eternal life, Grant that we beseech 
-       Thee while meditating upon these mysteries of the Most Holy 
-       Rosary of the Blessed Virgin Mary, we may both imitate what 
-       they contain and obtain what they promise, through the same 
-       Christ our Lord Amen.
-
-
-       """)
+    print(closingPrayer)
 
 elif weekday == 2 or weekday == 6:
     # Glorious Mysteries
@@ -322,144 +196,42 @@ elif weekday == 2 or weekday == 6:
     input("Press Enter to Continue")
     print("\n" * 100)
 
-    print("""
-    
-    I believe in God, the Father Almighty, Creator of heaven and 
-    earth; and in Jesus Christ, His only Son, our Lord; Who was 
-    conceived by the Holy Spirit, born of the Virgin Mary, 
-    suffered under Pontius Pilate, was crucified, died, and was 
-    buried. He descended into hell; the third day He arose again 
-    from the dead. He ascended into heaven, and sits at the right 
-    hand of God, the Father Almighty; from thence He shall come 
-    to judge the living and the dead. I believe in the Holy 
-    Spirit, the Holy Catholic Church, the communion of Saints, 
-    the forgiveness of sins, the resurrection of the body and 
-    life everlasting. Amen. 
-
-
-        """)
+    print(creed)
     input("Press Enter to Continue")
     print("\n" * 100)
 
-    print("""
-    
-    Our Father who art in Heaven, 
-    Hallowed be thy name; 
-    Thy kingdom come 
-    Thy will be done 
-    On earth as it is in heaven. 
-    Give us this day our daily bread; 
-    And forgive us our trespasses 
-    As we forgive those who trespass against us; 
-    And lead us not into temptation, 
-    But deliver us from evil.
-    
-
-        """)
+    print(ourFather)
     input("Press Enter to Continue")
     print("\n" * 100)
 
-    print("""
-
-    Hail Mary, full of grace.
-    Our Lord is with thee.
-    Blessed art thou among women,
-    and blessed is the fruit of thy womb, Jesus.
-    Holy Mary, Mother of God,
-    pray for us sinners,
-    now and at the hour of our death.
-    Amen. 
-
-
-    """)
+    print(hailMary)
     input("Press Enter to Continue")
     print("\n" * 100)
 
-    print("""
-
-    Hail Mary, full of grace.
-    Our Lord is with thee.
-    Blessed art thou among women,
-    and blessed is the fruit of thy womb, Jesus.
-    Holy Mary, Mother of God,
-    pray for us sinners,
-    now and at the hour of our death.
-    Amen. 
-
-
-    """)
+    print(hailMary)
     input("Press Enter to Continue")
     print("\n" * 100)
 
-    print("""
-
-    Hail Mary, full of grace.
-    Our Lord is with thee.
-    Blessed art thou among women,
-    and blessed is the fruit of thy womb, Jesus.
-    Holy Mary, Mother of God,
-    pray for us sinners,
-    now and at the hour of our death.
-    Amen. 
-
-
-    """)
+    print(hailMary)
     input("Press Enter to Continue")
     print("\n" * 100)
 
-    print("""
-    
-    Glory be to the Father,
-    And to the Son,
-    And to the Holy Spirit.
-    As it was in the beginning, is now,
-    And ever shall be,
-    World without end. Amen.
-    
-
-    """)
+    print(gloryBe)
+    input("Press Enter to Continue")
+    print("\n" * 100)
 
     #
     # ADD THE FIVE DECADES
     #
 
-    
     input("Press Enter to Continue")
     print("\n" * 100)
 
-    print("""
-
-    Hail, Holy Queen
-    Hail, holy Queen, Mother of Mercy!
-    Our life, our sweetness, and our hope!
-    To thee do we cry, poor banished children of Eve, 
-       to thee do we send up our sighs, mourning and weeping 
-       in this valley, of tears.
-    Turn, then, most gracious advocate, thine eyes of 
-       mercy toward us; and after this our exile show unto us 
-       the blessed fruit of thy womb Jesus;
-    O clement, O loving, O sweet virgin Mary.
-            
-    Pray for us, O holy Mother of God
-    That we may be made worthy of the promises of Christ.
-
-
-    """)
-
+    print(salveRegina)
     input("Press Enter to Continue")
     print("\n" * 100)
 
-    print("""
-
-    O God whose only begotten Son by His Life has purchased 
-       for us the rewards of eternal life, Grant that we beseech 
-       Thee while meditating upon these mysteries of the Most Holy 
-       Rosary of the Blessed Virgin Mary, we may both imitate what 
-       they contain and obtain what they promise, through the same 
-       Christ our Lord Amen.
-
-
-       """)
+    print(closingPrayer)
 
 else:
     # Luminous Mysteries
@@ -472,142 +244,38 @@ else:
     input("Press Enter to Continue")
     print("\n" * 100)
 
-    print("""
-    
-    I believe in God, the Father Almighty, Creator of heaven and 
-    earth; and in Jesus Christ, His only Son, our Lord; Who was 
-    conceived by the Holy Spirit, born of the Virgin Mary, 
-    suffered under Pontius Pilate, was crucified, died, and was 
-    buried. He descended into hell; the third day He arose again 
-    from the dead. He ascended into heaven, and sits at the right 
-    hand of God, the Father Almighty; from thence He shall come 
-    to judge the living and the dead. I believe in the Holy 
-    Spirit, the Holy Catholic Church, the communion of Saints, 
-    the forgiveness of sins, the resurrection of the body and 
-    life everlasting. Amen. 
-
-
-        """)
+    print(creed)
     input("Press Enter to Continue")
     print("\n" * 100)
 
-    print("""
-    
-    Our Father who art in Heaven, 
-    Hallowed be thy name; 
-    Thy kingdom come 
-    Thy will be done 
-    On earth as it is in heaven. 
-    Give us this day our daily bread; 
-    And forgive us our trespasses 
-    As we forgive those who trespass against us; 
-    And lead us not into temptation, 
-    But deliver us from evil.
-    
-
-        """)
+    print(ourFather)
     input("Press Enter to Continue")
     print("\n" * 100)
 
-    print("""
-
-    Hail Mary, full of grace.
-    Our Lord is with thee.
-    Blessed art thou among women,
-    and blessed is the fruit of thy womb, Jesus.
-    Holy Mary, Mother of God,
-    pray for us sinners,
-    now and at the hour of our death.
-    Amen. 
-
-
-    """)
+    print(hailMary)
     input("Press Enter to Continue")
     print("\n" * 100)
 
-    print("""
-
-    Hail Mary, full of grace.
-    Our Lord is with thee.
-    Blessed art thou among women,
-    and blessed is the fruit of thy womb, Jesus.
-    Holy Mary, Mother of God,
-    pray for us sinners,
-    now and at the hour of our death.
-    Amen. 
-
-
-    """)
+    print(hailMary)
     input("Press Enter to Continue")
     print("\n" * 100)
 
-    print("""
-
-    Hail Mary, full of grace.
-    Our Lord is with thee.
-    Blessed art thou among women,
-    and blessed is the fruit of thy womb, Jesus.
-    Holy Mary, Mother of God,
-    pray for us sinners,
-    now and at the hour of our death.
-    Amen. 
-
-
-    """)
+    print(hailMary)
     input("Press Enter to Continue")
     print("\n" * 100)
 
-    print("""
-    
-    Glory be to the Father,
-    And to the Son,
-    And to the Holy Spirit.
-    As it was in the beginning, is now,
-    And ever shall be,
-    World without end. Amen.
-    
-
-    """)
+    print(gloryBe)
 
     #
     # ADD THE FIVE DECADES
     #
 
-    
     input("Press Enter to Continue")
     print("\n" * 100)
 
-    print("""
-
-    Hail, Holy Queen
-    Hail, holy Queen, Mother of Mercy!
-    Our life, our sweetness, and our hope!
-    To thee do we cry, poor banished children of Eve, 
-       to thee do we send up our sighs, mourning and weeping 
-       in this valley, of tears.
-    Turn, then, most gracious advocate, thine eyes of 
-       mercy toward us; and after this our exile show unto us 
-       the blessed fruit of thy womb Jesus;
-    O clement, O loving, O sweet virgin Mary.
-            
-    Pray for us, O holy Mother of God
-    That we may be made worthy of the promises of Christ.
-
-
-    """)
-
+    print(salveRegina)
     input("Press Enter to Continue")
     print("\n" * 100)
 
-    print("""
-
-    O God whose only begotten Son by His Life has purchased 
-       for us the rewards of eternal life, Grant that we beseech 
-       Thee while meditating upon these mysteries of the Most Holy 
-       Rosary of the Blessed Virgin Mary, we may both imitate what 
-       they contain and obtain what they promise, through the same 
-       Christ our Lord Amen.
-
-
-       """)
+    print(closingPrayer)
 
